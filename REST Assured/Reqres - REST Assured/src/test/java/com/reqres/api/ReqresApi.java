66e49a70c182c610de96c6779e8f9ae85c5c7af3
petyakrzhelyazkova <petya.kr.zhelyazkova@gmail.com>
2025-10-19 18:18:49 +0300

    Changing REST organization structure.

--- REST Assured/Reqres - REST Assured/src/test/java/com/reqres/api/ReqresApi.java
+++
@@ -1,9 +0,0 @@
-package com.reqres.api;
-
-import testframework.PropertiesManager;
-
-public class ReqresApi extends BaseApiService {
-    public ReqresApi(String subPath) {
-        super(subPath, PropertiesManager.getConfigProperties().getProperty("reqres.baseUrl"));
-    }
-}
--- REST Assured/Reqres - REST Assured/src/test/java/com/reqres/api/model/SingleUserResponse.java
+++
@@ -1,8 +0,0 @@
-package com.reqres.api.model;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class SingleUserResponse {
-    public User data;
-}
